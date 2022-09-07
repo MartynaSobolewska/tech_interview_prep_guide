420f5d046e217bbc0ba47a88eaa173a64825411f
MartynaSobolewskaExe <ms1143@exeter.ac.uk>
2022-09-07 23:32:01 +0100

    leet C++: two-sum

--- leetcode/C++/easy/two sum/2sum.cpp
+++ leetcode/C++/easy/two sum/2sum.cpp
@@ -1,19 +1,29 @@
 #include <map>
 #include <vector>
+using namespace std;
 
 class Solution {
     public:
         vector<int> twoSum(vector<int>& nums, int target) {
-            std::map<int, int> map;
-            for (int i = 0; i < nums.end(); i++){
-                map.insert({nums})
-                // TODO: finish
+            map<int, int> map;
+            int count = 0;
+            for (int i : nums){
+                printf("i = %d\n", i);
+                map.insert({i, count});
+                int pair = target - i;
+                if (map.count(pair)){
+                    printf("[%d, %d]\n", i, pair);
+                    vector<int> result {i, pair};
+                    return result;
+                }
             }
+            vector<int> vect {};
+            return vect;
         }
-}
+};
 
 int main(int argc, char **argv){
     Solution s;
-    vector<int> vect{ 10, 20, 30, 2, 1, 8, 4 };
-    s.twoSum(*vect, 24)
+    vector<int> vect { 10, 20, 30, 2, 1, 8, 4 };
+    s.twoSum(vect, 24);
 }
